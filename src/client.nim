633c8368d5f33b127b2e832194f5f8691f0f1e87
Juan Carlos <juancarlospaco@gmail.com>
2024-05-16 21:17:34 -0300

    init

--- src/client.nim
+++ src/client.nim
@@ -32,17 +32,17 @@ type
 
 proc close*(self: SyncFunctionsClient | AsyncFunctionsClient) = self.client.close()
 
-proc newSyncFunctionsClient*(url, apiKey: string): SyncFunctionsClient {.inline.} =
-  SyncFunctionsClient(url: url, client: newHttpClient(userAgent="supabase-nim/functions-nim v" & NimVersion,
+proc newSyncFunctionsClient*(url, apiKey: string; maxRedirects = 9.Positive; timeout = -1; proxy: Proxy = nil): SyncFunctionsClient {.inline.} =
+  SyncFunctionsClient(url: url, client: newHttpClient(userAgent="supabase/functions-nim v" & NimVersion, maxRedirects=maxRedirects, timeout=timeout, proxy=proxy,
     headers=newHttpHeaders({"Content-Type": "application/json", "Authorization": "Bearer " & apiKey})
   ))
 
-proc newAsyncFunctionsClient*(url, apiKey: string): AsyncFunctionsClient {.inline.} =
-  AsyncFunctionsClient(url: url, client: newAsyncHttpClient(userAgent="supabase-nim/functions-nim v" & NimVersion,
+proc newAsyncFunctionsClient*(url, apiKey: string; maxRedirects = 9.Positive; timeout = -1; proxy: Proxy = nil): AsyncFunctionsClient {.inline.} =
+  AsyncFunctionsClient(url: url, client: newAsyncHttpClient(userAgent="supabase/functions-nim v" & NimVersion, maxRedirects=maxRedirects, proxy=proxy,
     headers=newHttpHeaders({"Content-Type": "application/json", "Authorization": "Bearer " & apiKey})
   ))
 
-proc invoke*(self: SyncFunctionsClient | AsyncFunctionsClient; functionName: string, body = ""; responseType = SFResponseType.Json; region = SFRegion.Any; httpMethod = HttpPost; apiKey = ""): auto =
+proc invoke*(self: SyncFunctionsClient | AsyncFunctionsClient; functionName: string, body = ""; responseType = SFResponseType.Json; region = SFRegion.Any; httpMethod = HttpPost; multipart: MultipartData = nil; apiKey = ""): auto =
   # https://github.com/supabase/functions-js/blob/19512a44aa3b8e4ea89a825899a4e1b2223368af/src/FunctionsClient.ts#L37-L39
   if apiKey.len > 100: self.client.headers["Authorization"] = "Bearer " & apiKey
   # https://github.com/supabase/functions-js/blob/098537a0f5e1c2b2aca8891625c4deca846b0591/src/FunctionsClient.ts#L85-L86
@@ -50,4 +50,4 @@ proc invoke*(self: SyncFunctionsClient | AsyncFunctionsClient; functionName: str
   # https://github.com/supabase/functions-js/blob/098537a0f5e1c2b2aca8891625c4deca846b0591/src/FunctionsClient.ts#L60-L62
   if region != SFRegion.Any: self.client.headers["x-region"] = $region
   # https://github.com/supabase/functions-js/blob/19512a44aa3b8e4ea89a825899a4e1b2223368af/src/types.ts#L70
-  result = self.client.request(url = self.url & '/' & functionName, httpMethod = httpMethod, body = body)
+  result = self.client.request(url = self.url & '/' & functionName, httpMethod = httpMethod, body = body, multipart = multipart)
